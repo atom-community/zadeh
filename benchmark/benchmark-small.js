79344c2347f665715bfafecded5b2193e87ecd69
rajendrant <rajendrant@chromium.org>
2020-09-01 11:55:09 -0700

    Add test for small data

---
+++ benchmark/benchmark-small.js
@@ -0,0 +1,55 @@
+require('coffeescript/register')
+const fs = require('fs')
+const path = require('path')
+
+const performance = (require('perf_hooks')).performance
+
+const fuzzaldrinPlus = require('../fuzzaldrin')
+const legacy = require('fuzzaldrin-plus')
+
+const lines = fs.readFileSync(path.join(__dirname, 'data-small.txt'), 'utf8').trim().split('\n')
+const forceAllMatch = {
+  maxInners: -1
+}
+const mitigation = {
+  maxInners: Math.floor(0.2 * lines.length)
+}
+
+// warmup + compile
+fuzzaldrinPlus.filter(lines, 'index', forceAllMatch)
+legacy.filter(lines, 'index')
+
+function doFilterTest(query) {
+  console.log(`====== Running test - query:${query} ======`)
+  let startTime = performance.now()
+  const res_actual = fuzzaldrinPlus.filter(lines, query)
+  const elapsed = Math.round(performance.now() - startTime)
+
+  startTime = performance.now()
+  const res_expected = legacy.filter(lines, query)
+  const elapsed_legacy = Math.round(performance.now() - startTime)
+
+  /*
+    if (res_actual != res_expected) {
+      console.log(`=== TEST FAILED - query:${query}`)
+      console.log('Actual   : ', res_actual)
+      console.log('Expected : ', res_expected)
+    }*/
+  console.log(`Elapsed time - fuzzaldrin-plus-fast: ${elapsed} ms vs. fuzzaldrin-plus: ${elapsed_legacy} ms`)
+  if (elapsed > elapsed_legacy) {
+    console.error(`====== fuzzaldrin-plus-fast is SLOWER`)
+  }
+  console.log('')
+}
+
+doFilterTest('nm')
+doFilterTest('npm')
+doFilterTest('node')
+doFilterTest('grunt')
+doFilterTest('html')
+doFilterTest('doc')
+doFilterTest('cli')
+doFilterTest('js')
+doFilterTest('jas')
+doFilterTest('mine')
+doFilterTest('stream')
