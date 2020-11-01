a623b0ba9e6dd1e7f91100cbd9b33ab2fd7b5a59
aminya <aminyahyaabadi74@gmail.com>
2020-11-01 00:30:55 -0500

    Tree constructor

--- src/tree.cc
+++ src/tree.cc
@@ -12,4 +12,18 @@ struct Tree {
 	string dataKey;
 	string childrenKey;
 	bool hasChildren = false;
+
+	/** Parse a Tree object from JS */
+	Tree(const Napi::CallbackInfo& info) {
+		if (info.Length() != 3 || !info[0].IsObject() || !info[1].IsString() || !info[2].IsString()) {
+			Napi::TypeError::New(info.Env(), "Invalid arguments").ThrowAsJavaScriptException();
+			// default constructor
+		}
+		else {
+			tree = info[0].As<Napi::Object>();
+			dataKey = info[1].As<Napi::String>();
+			childrenKey = info[2].As<Napi::String>();
+		}
+	};
+
 };
