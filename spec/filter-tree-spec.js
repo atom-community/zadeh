9299b0851d8089319157c0880e2223f28e5fc7f2
aminya <aminyahyaabadi74@gmail.com>
2020-11-02 00:17:13 -0600

    add maxResults test

--- spec/filter-tree-spec.js
+++ spec/filter-tree-spec.js
@@ -48,6 +48,26 @@ describe("filterTree", () => {
           { data: '_bye4', index: 0, level: 1 }
         ]
       )).toBe(true)
+
+    // test maxResults
+    expect(DeepEqual(
+        filterTree(candidates, "bye", "data", "children", {maxResults: 2}),
+        [
+          { data: 'bye1', index: 0, level: 0 },
+          { data: '_bye4', index: 0, level: 1 },
+        ]
+      )).toBe(true)
+
+    expect(DeepEqual(
+        filterTree(candidates, "ye", "data", "children", {maxResults: 3}),
+        [
+          { data: 'eye', index: 2, level: 0 },
+          { data: 'bye1', index: 0, level: 0 },
+          { data: 'Bye2', index: 1, level: 0 },
+        ]
+      )).toBe(true)
+
+
   })
 
   it("can search in an array of children-less objects", () =>{
