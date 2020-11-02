d9a1a0bb77f9b6fdb601b1fb32adcc19002667b6
aminya <aminyahyaabadi74@gmail.com>
2020-11-01 20:19:40 -0600

    add CandidateObject to tree.h

--- src/tree.h
+++ src/tree.h
@@ -1,10 +1,19 @@
 #include <variant>
 
+struct CandidateObject {
+	CandidateString data;
+	size_t level = 0;
+	int32_t index = -1;
+
+	CandidateObject(CandidateString const data, size_t const level, int32_t const index);
+};
+
 template <typename T>
 struct Tree {
 	T jsTreeArrayOrObject;
 	string dataKey;
 	string childrenKey;
+	vector<CandidateObject> entriesArray;
 
 	Tree(T const _jsTreeArrayOrObject, string const _dataKey, string const _childrenKey) {};
 };
