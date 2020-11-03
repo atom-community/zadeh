d3d94af249239daaab24bf730bd217e299860789
aminya <aminyahyaabadi74@gmail.com>
2020-11-02 20:52:38 -0600

    chore: make kMaxThreads static

--- src/common.h
+++ src/common.h
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-const size_t kMaxThreads = 16;
+static const size_t kMaxThreads = 16;
 
 #ifdef ENABLE_DEBUG
 // Safe string class that logs error when index is accessed outside the string.
