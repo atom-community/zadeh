c56658f6b93ca832dddb9b69841f7860bdc6ee4d
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-07 05:37:59 -0600

    benchmark: add tree benchmarks

---
+++ benchmark/benchmark-tree.js
@@ -0,0 +1,59 @@
+const fs = require("fs")
+const path = require("path")
+const {averageArray} = require("./testutils")
+
+try {
+  performance = require("perf_hooks").performance
+} catch {
+  performance = window.performance
+}
+
+const { TreeFilterer, filterTree } = require("../fuzzaldrin-dist")
+
+const outlineData = JSON.parse(fs.readFileSync(path.join(__dirname, "tree.json"), "utf8"))
+
+// Loading using TreeFilterer
+
+let t1 = performance.now()
+const treeFilterer = new TreeFilterer()
+treeFilterer.setCandidates(outlineData, "plainText", "children")
+console.log(`\t TreeFilterer.setCandidates: ${(performance.now() - t1).toFixed(2)} ms`)
+
+// Filter using TreeFilterer
+let t2 = performance.now()
+const out_text = treeFilterer.filter("text")
+let t2_delta = performance.now() - t2
+console.log(`TreeFilterer.filter text: ${t2_delta.toFixed(2)} ms`)
+
+//
+let t3 = performance.now()
+const out_dips = treeFilterer.filter("dips")
+let t3_delta = performance.now() - t3
+console.log(`TreeFilterer.filter dips: ${t3_delta.toFixed(2)} ms`)
+
+//
+let t4 = performance.now()
+const out_disp = treeFilterer.filter("disp")
+let t4_delta = performance.now() - t4
+console.log(`TreeFilterer.filter disp: ${t4_delta.toFixed(2)} ms`)
+
+let t5 = performance.now()
+const out_txt = treeFilterer.filter("txt")
+let t5_delta = performance.now() - t5
+console.log(`TreeFilterer.filter txt: ${t5_delta.toFixed(2)} ms`)
+
+let t6 = performance.now()
+const out_getBuffer = treeFilterer.filter("getBuffer")
+let t6_delta = performance.now() - t6
+console.log(`TreeFilterer.filter getBuffer: ${t6_delta.toFixed(2)} ms`)
+
+const out_filterTree_txt = filterTree(outlineData, "txt", "plainText", "children")
+let t10_delta = performance.now() - t10
+console.log(`filterTree txt: ${t10_delta.toFixed(2)} ms`)
+
+let t11 = performance.now()
+const out_filterTree_getBuffer = filterTree(outlineData, "getBuffer", "plainText", "children")
+let t11_delta = performance.now() - t11
+console.log(`filterTree getBuffer: ${t11_delta.toFixed(2)} ms`)
+
+console.log(`\t filterTree average: ${averageArray([t7_delta,t8_delta,t9_delta,t10_delta,t11_delta]).toFixed(3)} ms`)
