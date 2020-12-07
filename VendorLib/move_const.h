34d592969802c668bd486deebae215b595fe2710
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-07 03:50:27 -0600

    prep: make move_const noexcept

--- VendorLib/move_const.h
+++ VendorLib/move_const.h
@@ -3,7 +3,7 @@
 #include <type_traits>
 
 template <typename T>
-typename std::remove_const<typename std::remove_reference<T>::type>::type&& move_const(T&& arg)
+typename std::remove_const<typename std::remove_reference<T>::type>::type&& move_const(T&& arg) noexcept
 {
     return const_cast<typename std::remove_const<typename std::remove_reference<T>::type>::type&&>(
         static_cast<typename std::remove_reference<T>::type&&>(arg)
