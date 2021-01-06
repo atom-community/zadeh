83f884e738cf566bba79d5ec38c791eed57b69e5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-05 23:29:58 -0600

    feat: use ArrayFilterer and TreeFilterer in node bindings

--- src/binding/node.cc
+++ src/binding/node.cc
@@ -10,54 +10,31 @@ Napi::Value Zadeh::Filter(const Napi::CallbackInfo &info) {
         Napi::TypeError::New(info.Env(), "Invalid arguments for Filter").ThrowAsJavaScriptException();
         return Napi::Boolean();
     }
-    // optimization for no candidates
-    if (candidates_.empty()) {
-        return Napi::Array::New(info.Env());
-    }
-    const std::string query = info[0].As<Napi::String>();
-    const size_t maxResults = info[1].As<Napi::Number>().Uint32Value();
-    const bool usePathScoring = info[2].As<Napi::Boolean>();
-    const bool useExtensionBonus = info[3].As<Napi::Boolean>();
-    const Options options(query, maxResults, usePathScoring, useExtensionBonus);
-    const auto matches = filter(candidates_, query, options);
 
-    for (uint32_t i = 0, len = matches.size(); i < len; i++) {
-        res[i] = Napi::Number::New(info.Env(), matches[i]);
+    const auto filter_indices = arrayFilterer.filter_indices(
+      info[0].As<Napi::String>(),
+      info[1].As<Napi::Number>().Uint32Value(),
+      info[2].As<Napi::Boolean>(),
+      info[3].As<Napi::Boolean>());
+
+    for (uint32_t i = 0, len = filter_indices.size(); i < len; i++) {
+        res[i] = Napi::Number::New(info.Env(), filter_indices[i]);
     }
     return res;
 }
 
-Napi::Value Zadeh::setArrayFiltererCandidates(const Napi::CallbackInfo &info) {
+Napi::Value ZadehNode::setArrayFiltererCandidates(const Napi::CallbackInfo &info) {
     if (info.Length() != 1 || !info[0].IsArray()) {
         Napi::TypeError::New(info.Env(), "Invalid arguments for setArrayFiltererCandidates").ThrowAsJavaScriptException();
         return Napi::Boolean();
     }
-    auto candidates = info[0].As<Napi::Array>();
-    const auto N = candidates.Length();
-    const auto num_chunks = N < 1000u * kMaxThreads ? N / 1000u + 1u : kMaxThreads;
 
-
-    candidates_.clear();
-    candidates_.resize(num_chunks);
-
-    auto cur_start = 0u;
-    for (auto i = 0u; i < num_chunks; i++) {
-
-        auto chunk_size = N / num_chunks;
-        // Distribute remainder among the chunks.
-        if (i < N % num_chunks) {
-            chunk_size++;
-        }
-        for (auto j = cur_start; j < cur_start + chunk_size; j++) {
-            candidates_[i].emplace_back(candidates.Get(j).ToString().Utf8Value());
-        }
-        cur_start += chunk_size;
-    }
+    arrayFilterer.set_candidates(info[0].As<Napi::Array>());
 
     return Napi::Boolean();
 }
 
-Napi::Value Zadeh::setTreeFiltererCandidates(const Napi::CallbackInfo &info) {
+Napi::Value ZadehNode::setTreeFiltererCandidates(const Napi::CallbackInfo &info) {
     // parse arguments
     if (info.Length() != 3
         || !info[0].IsArray()
@@ -65,41 +42,18 @@ Napi::Value Zadeh::setTreeFiltererCandidates(const Napi::CallbackInfo &info) {
         Napi::TypeError::New(info.Env(), "Invalid arguments for setTreeFiltererCandidates").ThrowAsJavaScriptException();
         return Napi::Boolean();
     }
-    const auto jsTreeArray = info[0].As<Napi::Array>();
-    const string dataKey = info[1].As<Napi::String>();
-    const string childrenKey = info[2].As<Napi::String>();
 
     // create Tree and set candidates
-    _tree = Tree(jsTreeArray, dataKey, childrenKey);
-
-    const auto &candidates = _tree.entriesArray;
-
-    const auto N = candidates.size();    // different
-    const auto num_chunks = N < 1000u * kMaxThreads ? N / 1000u + 1u : kMaxThreads;
-
-
-    candidates_.clear();
-    candidates_.resize(num_chunks);
-
-    auto cur_start = 0u;
-    for (auto i = 0u; i < num_chunks; i++) {
-        auto chunk_size = N / num_chunks;
-        // Distribute remainder among the chunks.
-        if (i < N % num_chunks) {
-            chunk_size++;
-        }
-        for (auto j = cur_start; j < cur_start + chunk_size; j++) {
-            candidates_[i].emplace_back(candidates[j].data);    // different // TODO copy
-        }
-        cur_start += chunk_size;
-    }
-
+    treeFilterer.set_candidates(
+      info[0].As<Napi::Array>(),
+      info[1].As<Napi::String>(),
+      info[2].As<Napi::String>());
 
     return Napi::Boolean();
 }
 
 /** (query: string, maxResults: number, usePathScoring: bool, useExtensionBonus: bool) */
-Napi::Value Zadeh::FilterTree(const Napi::CallbackInfo &info) {
+Napi::Value ZadehNode::FilterTree(const Napi::CallbackInfo &info) {
     // parse arguments
     if (info.Length() != 4
         || !info[0].IsString()
@@ -107,28 +61,17 @@ Napi::Value Zadeh::FilterTree(const Napi::CallbackInfo &info) {
         Napi::TypeError::New(info.Env(), "Invalid arguments for FilterTree").ThrowAsJavaScriptException();
         return Napi::Array::New(info.Env());
     }
-    // optimization for no candidates
-    if (candidates_.empty()) {
-        return Napi::Array::New(info.Env());
-    }
 
-    // parse query
-    const std::string query = info[0].As<Napi::String>();
+    const auto filter_indices = treeFilterer.filter_indices(
+      info[0].As<Napi::String>(),
+      info[1].As<Napi::Number>().Uint32Value(),
+      info[2].As<Napi::Boolean>(),
+      info[3].As<Napi::Boolean>());
 
-    // parse options
-    const size_t maxResults = info[1].As<Napi::Number>().Uint32Value();
-    const bool usePathScoring = info[2].As<Napi::Boolean>();
-    const bool useExtensionBonus = info[3].As<Napi::Boolean>();
-
-    // create options
-    const auto options = Options(query, maxResults, usePathScoring, useExtensionBonus);
-
-    // perform filtering
-    const auto matches = filter(candidates_, query, options);
 
     auto filteredCandidateObjects = Napi::Array::New(info.Env());    // array of candidate objects (with their address in index and level)
-    for (uint32_t i = 0, len = matches.size(); i < len; i++) {
-        auto &entry = _tree.entriesArray[matches[i]];    //
+    for (uint32_t i = 0, len = filter_indices.size(); i < len; i++) {
+        auto &entry = treeFilterer.candidates_vector[filter_indices[i]];    //
 
         // create {data, index, level}
         auto obj = Napi::Object::New(info.Env());
@@ -195,21 +138,21 @@ Napi::String wrap(const Napi::CallbackInfo &info) {
     return Napi::String::New(info.Env(), res);
 }
 
-Napi::Object Zadeh::Init(Napi::Env env, Napi::Object exports) {
+Napi::Object ZadehNode::Init(Napi::Env env, Napi::Object exports) {
     Napi::HandleScope scope(env);
 
-    // define Zadeh class in JS
+    // define ZadehNode class in JS
     const auto func = DefineClass(
       env,
       "Zadeh",
       { // member functions in JS
-        InstanceMethod("filter", &Zadeh::Filter),
-        InstanceMethod("filterTree", &Zadeh::FilterTree),
-        InstanceMethod("setArrayFiltererCandidates", &Zadeh::setArrayFiltererCandidates),
-        InstanceMethod("setTreeFiltererCandidates", &Zadeh::setTreeFiltererCandidates)
+        InstanceMethod("filter", &ZadehNode::Filter),
+        InstanceMethod("filterTree", &ZadehNode::FilterTree),
+        InstanceMethod("setArrayFiltererCandidates", &ZadehNode::setArrayFiltererCandidates),
+        InstanceMethod("setTreeFiltererCandidates", &ZadehNode::setTreeFiltererCandidates)
 
       });
-    // export Zadeh class to JS
+    // export ZadehNode class to JS
     exports.Set("Zadeh", func);
 
     exports.Set("score", Napi::Function::New(env, score));
@@ -219,7 +162,7 @@ Napi::Object Zadeh::Init(Napi::Env env, Napi::Object exports) {
 }
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-    return Zadeh::Init(env, exports);
+    return ZadehNode::Init(env, exports);
 }
 
 NODE_API_MODULE(NODE_GYP_MODULE_NAME, InitAll)
--- src/binding/node.h
+++ src/binding/node.h
@@ -1,28 +1,20 @@
-#pragma once
-#ifndef Zadeh_H
-#define Zadeh_H
+#ifndef Zadeh_Node_H
+#define Zadeh_Node_H
 
 #include <napi.h>
 
-#include "../common.h"
-#include "../options.h"
-#include "../scorer.h"
-#include "../matcher.h"
-#include "../path_scorer.h"
-#include "../query.h"
-#include "../filter.h"
-
-#include "tree.h"
+#include "../zadeh.h"
 
 namespace zadeh {
+
 
 // Converted from the example at
 // https://github.com/nodejs/node-addon-examples/blob/master/6_object_wrap/node-addon-api/
 
-class Zadeh : public Napi::ObjectWrap<Zadeh> {
+class ZadehNode : public Napi::ObjectWrap<ZadehNode> {
   public:
     static Napi::Object Init(Napi::Env env, Napi::Object exports);
-    explicit Zadeh(const Napi::CallbackInfo &info) : Napi::ObjectWrap<Zadeh>(info) {}
+    explicit ZadehNode(const Napi::CallbackInfo &info) : Napi::ObjectWrap<ZadehNode>(info) {}
 
     Napi::Value Filter(const Napi::CallbackInfo &info);
     Napi::Value setArrayFiltererCandidates(const Napi::CallbackInfo &info);
@@ -31,10 +23,10 @@ class Zadeh : public Napi::ObjectWrap<Zadeh> {
     Napi::Value FilterTree(const Napi::CallbackInfo &info);
 
   private:
-    vector<std::vector<CandidateString>> candidates_{};
-    Tree _tree;
+    ArrayFilterer<Napi::Array, CandidateString> arrayFilterer{};
+    TreeFilterer<Napi::Array, Napi::Object> treeFilterer{};
 };
 
 
 }    // namespace zadeh
-#endif    // Zadeh_H
+#endif    // Zadeh_Node_H
