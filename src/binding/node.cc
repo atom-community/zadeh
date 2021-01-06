ecf22b2f7741faa0439fd173332e32573b0f9123
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-05 23:30:22 -0600

    feat: add the data interface for node bindings

--- src/binding/node.cc
+++ src/binding/node.cc
@@ -4,7 +4,47 @@
 
 namespace zadeh {
 
-Napi::Value Zadeh::Filter(const Napi::CallbackInfo &info) {
+// Node Array Data Interface
+template<>
+string get_at(const Napi::Array &candidates, const uint32_t j) {
+    return candidates.Get(j).ToString().Utf8Value();
+}
+
+template<>
+Napi::Object get_at(const Napi::Array &candidates, const uint32_t j) {
+    return candidates.Get(j).As<Napi::Object>();
+}
+
+template<>
+size_t get_size(const Napi::Array &candidates) {
+    return candidates.Length();
+}
+
+// Node Object Data Interface
+template<>
+CandidateString get_at(const Napi::Object &candidates, const string j) {
+    return candidates.Get(j).ToString().Utf8Value();
+}
+
+
+/** Get the children of a tree_object (Napi::Object) */
+template<>
+optional<Napi::Array> get_children(const Napi::Object &tree_object, const string &children_key) {
+    // determine if it has children
+    if (tree_object.HasOwnProperty(children_key)) {
+        const auto childrenRaw = tree_object.Get(children_key);
+        if (childrenRaw.IsArray()) {
+            const auto childrenArray = childrenRaw.As<Napi::Array>();
+            if (childrenArray.Length() != 0) {
+                return childrenArray;
+            }
+        }
+    }
+    return {};
+}
+
+
+Napi::Value ZadehNode::Filter(const Napi::CallbackInfo &info) {
     auto res = Napi::Array::New(info.Env());
     if (info.Length() != 4 || !info[0].IsString() || !info[1].IsNumber() || !info[2].IsBoolean() || !info[3].IsBoolean()) {
         Napi::TypeError::New(info.Env(), "Invalid arguments for Filter").ThrowAsJavaScriptException();
