939cfa02dbdb53eeb09a5bcddd5e904d2254fb89
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-25 22:42:19 -0500

    chore: move options types to node.ts

--- src/binding/node.ts
+++ src/binding/node.ts
@@ -1,7 +1,40 @@
 import nodeGypBuld from "node-gyp-build"
 const binding = nodeGypBuld(__dirname) // this relies on Parcel to bundle this file in the root of the package, so __dirname becomes correct
 
-import type { IOptions, IFilterOptions } from "./node-types"
+export interface IOptions {
+  /** @default false */
+  allowErrors?: boolean
+
+  /** @default true */
+  usePathScoring?: boolean
+
+  /** @default false */
+  useExtensionBonus?: boolean
+
+  pathSeparator?: "/" | "\\" | string
+
+  // TODO not implemented?
+  // optCharRegEx?: RegExp
+
+  // TODO not implemented?
+  // wrap?: { tagOpen?: string; tagClass?: string; tagClose?: string }
+
+  /** @deprecated: there is no major benefit by precomputing something just for the query. */
+  preparedQuery?: {}
+}
+
+export type IFilterOptions<T> = IOptions & {
+  /** @deprecated The key to use when candidates is an object
+   * Deprecated option. Pass the key as a string to the second argument of 'ArrayFilterer.setCandidates' or to the third argument of 'filter'
+   */
+  key?: T extends string ? never : keyof T
+
+  /** The maximum numbers of results to return */
+  maxResults?: number
+
+  // TODO not implemented
+  // maxInners?: number
+}
 
 const defaultPathSeparator = process.platform === "win32" ? "\\" : "/"
 
