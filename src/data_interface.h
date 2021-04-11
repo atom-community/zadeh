affc93892044367d3cad1a748ac101a20655bf05
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-11 18:28:31 -0500

    feat: add init, set_at, get_children, get_ref, release_ref interfaces

--- src/data_interface.h
+++ src/data_interface.h
@@ -6,15 +6,35 @@
 namespace zadeh {
 
 
-// Data Interface
+/** Data Interface */
 
+/** Initialize array */
+template<typename ArrayType, typename AllocatorType, typename SizeType = size_t>
+ArrayType init(const SizeType len, const AllocatorType &alloc);
+
+template<typename ReferenceType, typename ValueType>
+ReferenceType get_ref(const ValueType &value);
+
+// TODO do we need manual releasing?
+//template<typename ReferenceType>
+//void release_ref(ReferenceType reference);
+
+/** Index array */
 template<typename ArrayType, typename ElementType, typename IndexType = size_t>
 ElementType get_at(const ArrayType &candidates, const IndexType iCandidate);
 
+/** Get size of array */
 template<typename ArrayType, typename SizeType = size_t>
 SizeType get_size(const ArrayType &candidates);
 
-// vector<E>
+/** Set element of array */
+template<typename ArrayType, typename ElementType, typename IndexType = size_t>
+void set_at(ArrayType &candidates, ElementType &&value, const IndexType iCandidate);
+
+/** Get children of a tree */
+template<typename ParentType, typename ChildType>
+optional<ChildType> get_children(const ParentType &tree_object, const string &children_key);
+
 template<>
 CandidateString get_at(const vector<CandidateString> &candidates, const unsigned int iCandidate) {
     return candidates[iCandidate];
