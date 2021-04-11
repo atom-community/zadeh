aa3dae03adcbbb22a68420d2fdb03c76e5415ecc
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-11 16:51:06 -0500

    test: a StringArrayFilterer filters string arrays

--- test/string-array-filterer-spec.js
+++ test/string-array-filterer-spec.js
@@ -1,7 +1,12 @@
 const { StringArrayFilterer } = require("../index")
 
 describe("StringArrayFilterer", function () {
-  it("tilres string arrays", function () {
+  it("filters string arrays", function () {
+    const arrayFilterer = new StringArrayFilterer(["Call", "Me", "Maybe"])
+    expect(arrayFilterer.filter("me")).toEqual(["Me", "Maybe"])
+    expect(arrayFilterer.filter("all")).toEqual(["Call"])
+  })
+  it("can set candidates later", function () {
     const arrayFilterer = new StringArrayFilterer()
     arrayFilterer.setCandidates(["Call", "Me", "Maybe"]) // set candidates only once
     // call filter multiple times
