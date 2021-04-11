a61b841ff18a30f982f0b03ef008418be93bb80b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-11 18:32:17 -0500

    feat: add init, get_at, set_at, get_ref for Napi::Object

--- src/binding/node_data_interface.h
+++ src/binding/node_data_interface.h
@@ -40,6 +40,27 @@ Napi::Reference<Napi::Array> get_ref(const Napi::Array &arr) {
 //    arr.Unref();
 //}
 
+/** Napi::Object Data Interface */
+
+template<>
+Napi::Object init(const size_t len, const Napi::Env &env) {
+    return Napi::Object::New(env);
+}
+
+template<>
+CandidateString get_at(const Napi::Object &candidates, const string ind) {
+    return candidates.Get(ind).ToString().Utf8Value();
+}
+
+template<>
+void set_at(Napi::Array &candidates, CandidateString &&value, const size_t iCandidate) {
+    candidates.Set(iCandidate, move(value));
+}
+
+template<>
+Napi::Reference<Napi::Object> get_ref(const Napi::Object &obj) {
+    return Napi::Persistent(obj);
+}
 
 /** Get the children of a tree_object (Napi::Object) */
 template<>
