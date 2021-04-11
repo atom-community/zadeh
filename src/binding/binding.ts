d1c4a677f96680a449351d1d4d19b1f7c9b6b9d9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-11 06:35:00 -0500

    fix: move validators from C++ to TypeScript

--- src/binding/binding.ts
+++ src/binding/binding.ts
@@ -27,3 +27,82 @@ export declare function score(
 export declare function match(str: string, query: string, pathSeparator: string): Array<number>
 
 export declare function wrap(str: string, query: string, pathSeparator: string): string
+
+// Argument validators
+
+export function validate_filter(...args: Parameters<Zadeh["filter"]>) {
+  if (
+    !(
+      typeof args[0] === "string" &&
+      typeof args[1] === "number" &&
+      typeof args[2] === "boolean" &&
+      typeof args[3] === "boolean"
+    )
+  ) {
+    throw new Error(`Invalid arguments for filter: ${args}`)
+  }
+}
+
+export function validate_setArrayFiltererCandidates(...args: Parameters<Zadeh["setArrayFiltererCandidates"]>) {
+  if (!Array.isArray(args[0])) {
+    throw new Error(`Invalid arguments for setArrayFiltererCandidates: ${args}`)
+  }
+}
+
+export function validate_filterTree(...args: Parameters<Zadeh["filterTree"]>) {
+  if (
+    !(
+      typeof args[0] === "string" &&
+      typeof args[1] === "number" &&
+      typeof args[2] === "boolean" &&
+      typeof args[3] === "boolean"
+    )
+  ) {
+    throw new Error(`Invalid arguments for filterTree: ${args}`)
+  }
+}
+
+export function validate_setTreeFiltererCandidates(...args: Parameters<Zadeh["setTreeFiltererCandidates"]>) {
+  if (!Array.isArray(args[0])) {
+    throw new Error(`Invalid arguments for setTreeFiltererCandidates: ${args}`)
+  }
+}
+
+export function validate_score(...args: Parameters<typeof score>) {
+  if (
+    !(
+      typeof args[0] === "string" &&
+      typeof args[1] === "string" &&
+      typeof args[2] === "boolean" &&
+      typeof args[3] === "boolean"
+    )
+  ) {
+    throw new Error(`Invalid arguments for score: ${args}`)
+  }
+}
+
+export function validate_match(...args: Parameters<typeof match>) {
+  if (
+    !(
+      typeof args[0] === "string" &&
+      typeof args[1] === "string" &&
+      typeof args[2] === "string" &&
+      /** PathSeparator */ args[2].length === 1
+    )
+  ) {
+    throw new Error(`Invalid arguments for match: ${args}`)
+  }
+}
+
+export function validate_wrap(...args: Parameters<typeof wrap>) {
+  if (
+    !(
+      typeof args[0] === "string" &&
+      typeof args[1] === "string" &&
+      typeof args[2] === "string" &&
+      /** PathSeparator */ args[2].length === 1
+    )
+  ) {
+    throw new Error(`Invalid arguments for wrap: ${args}`)
+  }
+}
